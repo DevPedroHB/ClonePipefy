13597db881f622ddd863f34d55de521c87bca72e
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2020-11-14 18:15:55 -0300

    Layout e função drag n' drop

---
+++ frontend/src/components/Board/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import produce from 'immer';
+import { Container } from './styles';
+import { loadLists } from '../../services/api';
+import BoardContext from './context';
+import List from '../List';
+
+const data = loadLists();
+
+export default function Board(){
+    const [lists, setList] = useState(data);
+    function move(fromList, toList, from, to){
+        setList(produce(lists, draft => {
+            const dragged = draft[fromList].cards[from];
+            draft[fromList].cards.splice(from, 1);
+            draft[toList].cards.splice(to, 0, dragged);
+        }));
+    }
+    return(
+        <BoardContext.Provider value={{ lists, move }}>
+            <Container>
+                {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
+            </Container>
+        </BoardContext.Provider>
+    );
+}
